0bc3fa9c1d94783f3856f2a3816d0f6c7c6f4ada
Jaume Fàbrega P <jaumefapa@gmail.com>
2020-11-27 11:27:47 +0100

    todo component and style

---
+++ src/components/todo/index.js
@@ -0,0 +1,16 @@
+import "./index.css";
+
+const Todo = () => {
+  return (
+    <div className="todo">
+      <div className="content-todo">
+        <div>This is a todo</div>
+      </div>
+      <div className="delete-todo">
+        <span className="material-icons">delete</span>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
